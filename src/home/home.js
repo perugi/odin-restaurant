cdfd370cc05bb8d7ce85dafa24bbbc3e0b2bb124
perugi <dominik.perusko@gmail.com>
2023-11-22 22:54:35 +0100

    Separate content generation into separate file

---
+++ src/home/home.js
@@ -0,0 +1,17 @@
+import Slider from "./slider.jpg";
+
+export default function generateHome() {
+  const content = document.getElementById("content");
+
+  const heading = document.createElement("h1");
+  heading.textContent = "White Castle";
+  content.appendChild(heading);
+
+  const sliderImage = new Image();
+  sliderImage.src = Slider;
+  content.appendChild(sliderImage);
+
+  const description = document.createElement("p");
+  description.textContent = "Welcome to our restaurant!";
+  content.appendChild(description);
+}
