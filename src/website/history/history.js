7dfe91586cf75bd568f2f2db04292c623574415e
perugi <dominik.perusko@gmail.com>
2023-11-29 22:45:14 +0100

    Style history

--- src/website/history/history.js
+++ src/website/history/history.js
@@ -5,19 +5,24 @@ const history = {
 
   createSubpage: () => {
     const content = document.createElement("div");
+    content.classList.add("history");
+
+    const historyContent = document.createElement("div");
+    historyContent.classList.add("history-content");
+    content.appendChild(historyContent);
 
     const heading = document.createElement("h1");
     heading.textContent = "It began with an idea.";
-    content.appendChild(heading);
+    historyContent.appendChild(heading);
 
     const history = document.createElement("p");
     history.textContent =
       "In 1921, Billy Ingram launched a family-owned business with $700 and an idea, selling five-cent, small, square hamburgers so easy to eat, they were dubbed Sliders and sold by the sack. In 2014, Time® Magazine declared our iconic Original Slider® the most influential burger of all time. Today our family-owned business sells the same humble, 100 percent USDA beef patty with onions and pickle, creating memorable moments for Craver generations everywhere. The only thing better than polishing off a Sack of Sliders alone, is doing it with friends. Could it get any better than that? White Castle. Because The Crave Is A Powerful Thing®.";
-    content.appendChild(history);
+    historyContent.appendChild(history);
 
     const firstImage = new Image();
     firstImage.src = First;
-    content.appendChild(firstImage);
+    historyContent.appendChild(firstImage);
 
     return content;
   },
